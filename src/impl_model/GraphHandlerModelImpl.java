e766fca02723b8de4fe7c790f936ad50d40ba589
Daniel Hofmeister <de.slannesH@gmail.com>
2014-10-08 19:18:20 +0200

    Change load() in graphhandler

--- src/impl_model/GraphHandlerModelImpl.java
+++ src/impl_model/GraphHandlerModelImpl.java
@@ -3,20 +3,28 @@ package impl_model;
 import impl_controller.GKA;
 import interface_controller.FileHandler;
 import interface_model.GraphHandlerModel;
-import interface_model.NamedEdge;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jgrapht.ListenableGraph;
-import org.jgrapht.graph.DirectedMultigraph;
-import org.jgrapht.graph.Multigraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedPseudograph;
+import org.jgrapht.graph.DirectedWeightedPseudograph;
+import org.jgrapht.graph.ListenableDirectedGraph;
+import org.jgrapht.graph.ListenableDirectedWeightedGraph;
+import org.jgrapht.graph.ListenableUndirectedGraph;
+import org.jgrapht.graph.ListenableUndirectedWeightedGraph;
+import org.jgrapht.graph.Pseudograph;
+import org.jgrapht.graph.WeightedPseudograph;
 
 class GraphHandlerModelImpl implements GraphHandlerModel {
 
-	private FileHandler						_file_handler;
-	private	ListenableGraph<String, ?> 		_graph;
+	private final 		int 							DEFAULT_EDGE_WEIGHT = 1; 
+	private 			FileHandler						_file_handler;
+	private				ListenableGraph<String, ?> 		_graph;
 	
 	// Creation
 	public static GraphHandlerModel create() {
@@ -50,21 +58,154 @@ class GraphHandlerModelImpl implements GraphHandlerModel {
 	public void load() throws IOException {
 		_file_handler.load();
 		ArrayList<String> edges = _file_handler.get_content();
+		ArrayList<String> formatted_edges = format_edges(edges);
 		// Parse to ListenableGraph
+		String sz = "[^\\-><:()]"; // Sonderzeichen
+		Pattern reg = Pattern.compile("(?<v1>"+sz+"*)((?<richtung>[<-][->])(?<v2>"+sz+"*)"
+				+ "(\\((?<edgename>"+sz+"*)\\))?(:(?<edgeweight>"+sz+"*))?)?;");
 		
+		//Directed Graph
 		if(contains_once(edges, "->")) {
-			DirectedMultigraph<String, NamedWeightedEdge> accu;
-			String regex = "(\\w)+[--(\\w)+][((\\w)+)][:(\\w+)]"; 
-			
+					
+			//Directed weighted graph
+			if (contains_once(formatted_edges, ":")) {
+				DirectedWeightedPseudograph<String,NamedWeightedEdge> dir_weighted = new DirectedWeightedPseudograph<>(NamedWeightedEdge.class);
+				for (String s : edges) {
+					Matcher m = reg.matcher(s);
+					if (m.matches()) {
+						String node1 = m.group("v1");
+						String node2 = m.group("v2");
+						String direction = m.group("richtung");
+						String edgename = m.group("edgename");
+						int edgeweight = DEFAULT_EDGE_WEIGHT;
+						if (m.group("edgeweight") != null) {
+							edgeweight = Integer.parseInt(m.group("edgeweight"));
+						}
+						System.out.println("Node1:"+node1+"Node2"+node2);
+						if (node1 != null) {
+							dir_weighted.addVertex(node1);
+						}
+						if (node2 != null) {
+							dir_weighted.addVertex(node2);
+						}
+						NamedWeightedEdge e = null;
+						e = dir_weighted.addEdge(node1, node2);
+						dir_weighted.addEdge(node1, node2);
+						if (e != null) {
+							dir_weighted.setEdgeWeight(e, edgeweight);
+						}
+					} else {
+						System.out.println("Nicht valide Zeile: " + s);
+					}
+				}
+				ListenableGraph<String, NamedWeightedEdge> result = new ListenableDirectedWeightedGraph<String, NamedWeightedEdge>(dir_weighted);
+				set_graph(result);
+			}
+			//Directed Graph
+			else if (!contains_once(formatted_edges, ":")) {
+				DirectedPseudograph<String,DefaultEdge> dir = new DirectedPseudograph<>(DefaultEdge.class);
+				for (String s : edges) {
+					Matcher m = reg.matcher(s);
+					if (m.matches()) {
+						String node1 = m.group("v1");
+						String node2 = m.group("v2");
+						String direction = m.group("richtung");
+						System.out.println("Node1:"+node1+"Node2"+node2);
+						if (node1 != null) {
+							dir.addVertex(node1);
+						}
+						if (node2 != null) {
+							dir.addVertex(node2);
+						}
+						NamedWeightedEdge e = null;
+						if (node1 != null && node2 != null) {
+							dir.addEdge(node1, node2);
+						}
+					}
+					else {
+						System.out.println("Nicht valide Zeile:" + s);
+					}
+				}
+				ListenableGraph<String, DefaultEdge> result = new ListenableDirectedGraph<String, DefaultEdge>(dir);
+				set_graph(result);	
+			}
+		//Undirected Graph	
 		} else if(contains_once(edges, "--")) {
-			Multigraph<String, NamedWeightedEdge> accu;
-			String regex = "(\\w)+[->(\\w)+][((\\w)+)][:(\\w+)]";
-			
-		} else {
-			throw new IOException("Read unknown graph type.");
-		}		
+			//Undirected weighted graph
+			if (contains_once(formatted_edges, ":")) {
+				WeightedPseudograph<String,NamedWeightedEdge> undidr_weighted = new WeightedPseudograph<>(NamedWeightedEdge.class);
+				for (String s : edges) {
+					Matcher m = reg.matcher(s);
+					if (m.matches()) {
+						String node1 = m.group("v1");
+						String node2 = m.group("v2");
+						String direction = m.group("richtung");
+						String edgename = m.group("edgename");
+						int edgeweight = DEFAULT_EDGE_WEIGHT;
+						System.out.println(m.group("edgeweight"));
+						
+						if (m.group("edgeweight") != null) {
+							//String t = m.group("edgeweight").replace(" ", "");
+							edgeweight = Integer.parseInt(m.group("edgeweight").replace(" ",""));
+						}
+						System.out.println("Node1:"+node1+"Node2"+node2);
+						if (node1 != null) {
+							undidr_weighted.addVertex(node1);
+						}
+						if (node2 != null) {
+							undidr_weighted.addVertex(node2);
+						}
+						NamedWeightedEdge e = null;
+						e = undidr_weighted.addEdge(node1, node2);
+						undidr_weighted.addEdge(node1, node2);
+						if (e != null) {
+							undidr_weighted.setEdgeWeight(e, edgeweight);
+						}
+					} else {
+						System.out.println("Nicht valide Zeile:" + s);
+					}
+				}
+				ListenableGraph<String, NamedWeightedEdge> result = new ListenableUndirectedWeightedGraph<String, NamedWeightedEdge>(undidr_weighted);
+				set_graph(result);
+			}
+			//Undirected Graph
+			else if(!contains_once(formatted_edges, ":")) {
+				Pseudograph<String,DefaultEdge> dir = new Pseudograph<>(DefaultEdge.class);
+				for (String s : edges) {
+					Matcher m = reg.matcher(s);
+					if (m.matches()) {
+						String node1 = m.group("v1");
+						String node2 = m.group("v2");
+						String direction = m.group("richtung");
+						System.out.println("Node1:"+node1+"Node2"+node2);
+						if (node1 != null) {
+							dir.addVertex(node1);
+						}
+						if (node2 != null) {
+							dir.addVertex(node2);
+						}
+						NamedWeightedEdge e = null;
+						dir.addEdge(node1, node2);
+					} else {
+						System.out.println("Nicht valide Zeile:" + s);
+					}
+				}
+				ListenableGraph<String, DefaultEdge> result = new ListenableUndirectedGraph<String, DefaultEdge>(dir);
+				set_graph(result);	
+			}	
+		} 		
 	}
 	
+	private ArrayList<String> format_edges(ArrayList<String> edges) {
+		ArrayList<String> result = new ArrayList<String>();
+		for (String s : edges) {
+			String no_whitespace = s.replaceAll("\\s", "");
+			String formatted = no_whitespace.replace(";","");
+			result.add(formatted);
+		}
+		return result;		
+	}
+
 	private boolean contains_once(ArrayList<String> edges, String sign) {
 		if(edges == null) throw new NullPointerException();
 		for(String edge : edges) {
--- src/impl_tests/GraphHandlerImplTest.java
+++ src/impl_tests/GraphHandlerImplTest.java
@@ -1,14 +1,33 @@
 package impl_tests;
 
-import static org.junit.Assert.*;
+import impl_model.GKAModel;
+import interface_model.GraphHandlerModel;
 
+import java.io.IOException;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class GraphHandlerImplTest {
 
+	GraphHandlerModel gh;
+	
+	@Before
+	public void SetUp() {
+		gh = GKAModel.graphHandler();
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		try {
+			gh.load();
+			String s = gh.get_graph().edgeSet().toString();
+			System.out.println(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
