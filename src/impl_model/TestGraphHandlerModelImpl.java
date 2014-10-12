15b495fa164a9086f9a2da9fb1f58dc37f5a1606
Alex Mantel <alexmantel93@gmail.com>
2014-10-12 21:29:32 +0200

    Add tests for BreadthFirstModel helper methods

---
+++ src/impl_model/TestGraphHandlerModelImpl.java
@@ -0,0 +1,62 @@
+package impl_model;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DirectedPseudograph;
+import org.jgrapht.graph.Pseudograph;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGraphHandlerModelImpl {
+
+	/**
+	 * This test is for the helpermethods 
+	 */
+	
+	Graph<String, DefaultEdge> directed;
+	Graph<String, DefaultEdge> undirected;
+	
+	@Before
+	public void setUp() {
+		directed = new DirectedPseudograph<String, DefaultEdge>(DefaultEdge.class);
+		directed.addVertex("a");
+		directed.addVertex("b");
+		directed.addVertex("c");
+		directed.addVertex("d");
+		directed.addEdge("a", "b");
+		directed.addEdge("a", "c");
+		directed.addEdge("d", "a");
+		undirected = new Pseudograph<>(DefaultEdge.class);
+		undirected.addVertex("a");
+		undirected.addVertex("b");
+		undirected.addVertex("c");
+		undirected.addVertex("d");
+		undirected.addEdge("a", "b");
+		undirected.addEdge("a", "c");
+		undirected.addEdge("d", "a");
+	}
+	
+	@Test
+	public void testAccessibleAdjacentNodesDirected() {
+		BreadthFirstModelImpl bfs = (BreadthFirstModelImpl) BreadthFirstModelImpl.create(directed);
+		Set<String> expected = new HashSet<>();
+		expected.add("b");
+		expected.add("c");
+		assertEquals(expected, bfs.adjacentNodes("a"));
+	}
+
+	@Test
+	public void testAccessibleAdjacentNodesUndirected() {
+		BreadthFirstModelImpl bfs = (BreadthFirstModelImpl) BreadthFirstModelImpl.create(undirected);
+		Set<String> expected = new HashSet<>();
+		expected.add("b");
+		expected.add("c");
+		expected.add("d");
+		assertEquals(expected, bfs.adjacentNodes("a"));
+	}
+}
