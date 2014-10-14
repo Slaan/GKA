59d6a7395863446cda24b9f08e586b58d62ad9b1
Alex Mantel <alexmantel93@gmail.com>
2014-10-14 15:03:16 +0200

    Copy NamedWeightedEdge from GKAEdge
    https://github.com/JanaWengenroth/GKA1/blob/master/Project/src/GKA/Graph/GKAEdge.java

--- src/impl_model/NamedWeightedEdge.java
+++ src/impl_model/NamedWeightedEdge.java
@@ -1,19 +1,108 @@
 package impl_model;
 
+import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
-public class NamedWeightedEdge extends DefaultWeightedEdge {
+public class NamedWeightedEdge extends DefaultEdge {
 
-	@Override
-	public String toString() {
-		return Double.toString(this.getWeight());
+	private String edgeName = null;
+	private Double weight = null;
+	
+	public NamedWeightedEdge(String name, Double weight){
+		super();
+		this.edgeName = name;
+		this.weight = weight;
+	}
+	public Object getSource(){
+		return super.getSource();
 	}
 	
-	public String getSource() {
-		return (String) super.getSource();
+	public Object getTarget(){
+		return super.getTarget();
 	}
+	
+	public Double getWeight() {
+		return weight;
+	}
+	
+	public String getName(){
+		return this.edgeName;
+	}
+	
+    public String toString() {
+    	String retVal;
+    	if (edgeName == null){
+    		retVal = "(" + getSource() + " : " + getTarget() + ")";
+    	}
+    	else{
+    		retVal = "(" + edgeName + ")";
+    	}
+    	if (getWeight() != null){
+    		retVal += " : " + getWeight();
+    	}
+    	return retVal;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        if(object == null){
+            return false;
+        }
+        else if(object == this){
+            return true;
+        }
+        else if(!(object instanceof NamedWeightedEdge)){
+            return false;
+        }
+        else{
+            NamedWeightedEdge edge = (NamedWeightedEdge) object;
+            boolean retval = true;
+            if(getSource() == null){
+            	retval = retval && edge.getSource() == null;
+            }
+            else{
+            	retval = retval && getSource().equals(edge.getSource());
+            }
 
-	public String getTarget() {
-		return (String) super.getTarget();
-	}
+            if(getTarget() == null){
+            	retval = retval && edge.getTarget() == null;
+            }
+            else{
+            	retval = retval && getTarget().equals(edge.getTarget());
+            }
+            
+            if(getName() == null){
+            	retval = retval && edge.getName() == null;
+            }
+            else{
+            	retval = retval && getName().equals(edge.getName());
+            }
+            
+            if(getWeight() == null){
+            	retval = retval && edge.getWeight() == null;
+            }
+            else{
+            	retval = retval && getWeight().equals(edge.getWeight());
+            }
+            return retval;
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+    	int retVal = 31;
+    	if(getSource() != null){
+    		retVal += getSource().hashCode();
+        }
+    	if(getTarget() != null){
+    		retVal += getTarget().hashCode();
+        }
+    	if(getName() != null){
+    		retVal += getName().hashCode();
+        }
+    	if(getWeight() != null){
+    		retVal += getWeight().hashCode();
+        }
+        return retVal;
+    }
 }
